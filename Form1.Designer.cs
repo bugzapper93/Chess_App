c452b047faba5a9bbbe740a0323522d8326fa68a
biribiri <prettycool479@gmail.com>
2025-03-15 21:22:00 +0100

    Rendering board, relative positioning

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            groupBox1 = new GroupBox();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(160, 54);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(438, 469);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(870, 567);
+            Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
     }
 }
--- Variables.cs
+++ Variables.cs
@@ -9,6 +9,7 @@ namespace Chess_App
     public static class Variables
     {
         public const int Board_Size = 8;
-        public const int Square_Size = 50;
+        public const int Square_Size = 60;
+        public const int Margin = 10;
     }
 }
